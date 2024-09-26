f721cd14ed5c1a7723120e9df5ba32546e643df8
Gursimran Singh <simretsingh1313@gmail.com>
2024-09-26 09:15:54 +0530

    Updated README with visualizations and added images

--- Mysql.py
+++ Mysql.py
@@ -1,10 +1,14 @@
-pip install sql-connector-python
+# pip install sql-connector-python
 import mysql.connector
-myconn = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="root",
-  port=3306
-)
+myconn = mysql.connector.connect(host="localhost", user="root", password="root", port=3306,database="Joy")
+print(myconn)
+x=myconn.cursor()
+# print(x)
+# x.execute("create table first (name varchar(20),age int, gender varchar(10))")
+# x.execute("insert into first (name,age) values('Ram',30),('Shyam',25)")
 
-print(mydb)
+# x.execute("insert into first(genrer) values('M'),('M')")
+myconn.commit()
+x.execute("select * from first")
+for i in x:
+    print (i)
